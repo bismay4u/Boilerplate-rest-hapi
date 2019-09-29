bc07b90d1519985a9a7d6e308c3ff76debb84db6
Bismay Kumar Mohapatra <bismay4u@gmail.com>
2019-09-29 17:10:08 +0530

    Minor updates to packages and folders

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
-let Hapi = require('hapi')
+let Hapi = require('@hapi/hapi')
 let mongoose = require('mongoose')
 let RestHapi = require('rest-hapi')
 
-async function APIServer () {
-  try {
+async function APIServer() {
+    try {
 
-    let server = Hapi.Server({
-      port: 8999,
-      routes: {
-        validate: {
-          failAction: async (request, h, err) => {
-            RestHapi.logger.error(err);
-            throw err;
-          }
+        let server = Hapi.Server({
+            port: 8999,
+            routes: {
+                validate: {
+                    failAction: async(request, h, err) => {
+                        RestHapi.logger.error(err);
+                        throw err;
+                    }
+                }
+            }
+        })
+
+        let config = {
+            appTitle: 'MyAPI',
+            enableTextSearch: true,
+            logRoutes: true,
+            docExpansion: 'none',
+            swaggerHost: 'localhost:8080',
+            enableSwaggerUI: true,
+            mongo: {
+                URI: 'mongodb://localhost:27017/testAPI',
+            },
         }
-      }
-    })
 
-    let config = {
-      appTitle: 'MyAPI',
-      enableTextSearch: true,
-      logRoutes: true,
-      docExpansion: 'none',
-      swaggerHost: 'localhost:8080',
-      enableSwaggerUI: true,
-      mongo: {
-        URI: 'mongodb://localhost:27017/testAPI',
-      },
+        await server.register({
+            plugin: RestHapi,
+            options: {
+                mongoose: mongoose,
+                config: config,
+            },
+        })
+
+        await server.start()
+
+        RestHapi.logUtil.logActionComplete(RestHapi.logger, 'Server Initialized', server.info)
+
+        return server
+    } catch (err) {
+        console.log('Error starting server:', err)
     }
-
-    await server.register({
-      plugin: RestHapi,
-      options: {
-        mongoose: mongoose,
-        config: config,
-      },
-    })
-
-    await server.start()
-
-    RestHapi.logUtil.logActionComplete(RestHapi.logger, 'Server Initialized', server.info)
-
-    return server
-  } catch (err) {
-    console.log('Error starting server:', err)
-  }
 
 }
 
